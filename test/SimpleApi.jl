9c351199bdde9e13a254113cc8ff9457243c0609
aknudson <alexk.706@gmail.com>
2024-03-18 01:39:01 -0700

    added macros to test for interfaces

--- test/SimpleApi.jl
+++ test/SimpleApi.jl
@@ -1,60 +1,22 @@
 using Test
-using LinearAlgebra: issymmetric
 using NearestCorrelationMatrix
-using NearestCorrelationMatrix.Internals;
-
-
-function test_iscorrelation(X)
-    @test issquare(X)
-    @test issymmetric(X)
-    @test diagonals_are_one(X)
-    @test constrained_to_pm_one(X)
-    @test ispossemidef(X)
-end
-
-
-function test_alg(algType, msg)
-    @testset "$msg" begin
-        @testset "In-place" begin
-            for T in (Float64, Float32, Float16)
-                @testset "$T" begin
-                    r0 = get_negdef_matrix(T)
-                    alg = autotune(algType, NCMProblem(r0))
-
-                    r = copy(r0)
-                    @test_nowarn nearest_cor!(r, alg)
-
-                    @test eltype(r) === T
-                    @test r != r0
-                    @test iscorrelation(r0) == false
-                    test_iscorrelation(r)
-                end
-            end
-        end
-
-        @testset "Out-of-place" begin
-            for T in (Float64, Float32, Float16)
-                @testset "$T" begin
-                    r0 = get_negdef_matrix(T)
-                    alg = autotune(algType, NCMProblem(r0))
-
-                    @test_nowarn nearest_cor(r0, alg)
-
-                    r = nearest_cor(r0, alg)
-
-                    @test eltype(r) === T
-                    @test r != r0
-                    @test iscorrelation(r0) == false
-                    test_iscorrelation(r)
-                end
-            end
-        end
-    end
-end
 
 
 @testset "Simple API" begin
-    test_alg(Newton, "Newton Method")
-    test_alg(DirectProjection, "Direct Projection")
-    test_alg(AlternatingProjections, "Alternating Projections")
+    @test_isdefined nearest_cor
+    @test_isdefined nearest_cor!
+
+    r = rand(4, 4)
+
+    @test_isimplemented nearest_cor(r)
+    @test_isimplemented nearest_cor(r, Newton())
+    @test_isimplemented nearest_cor(r, Newton)
+
+    @test nearest_cor(r) isa AbstractMatrix
+
+    @test_isimplemented nearest_cor!(r)
+    @test_isimplemented nearest_cor!(r, Newton())
+    @test_isimplemented nearest_cor!(r, Newton)
+
+    @test nearest_cor!(r) isa AbstractMatrix
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,41 @@
 using Test, Aqua
 
 
+macro test_isimplemented(ex)
+    quote
+        try
+            $(esc(ex))
+        catch e
+            if e isa MethodError
+                @test false
+            else
+                rethrow()
+            end
+        else
+            @test true
+        end
+    end
+end
+
+macro test_isdefined(ex)
+    quote
+        try
+            $(esc(ex))
+        catch e
+            if e isa UndefVarError
+                @test false
+            else
+                rethrow()
+            end
+        else
+            @test true
+        end
+    end
+end
+
+
 include("Internals.jl")
+include("CommonSolveApi.jl")
 include("SimpleApi.jl")
 
 
