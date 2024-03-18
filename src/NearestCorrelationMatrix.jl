5024a09e930df165021b2ed2a5321e03d05a4086
aknudson <alexk.706@gmail.com>
2024-03-17 23:52:45 -0700

    added precompile statements back in

--- src/NearestCorrelationMatrix.jl
+++ src/NearestCorrelationMatrix.jl
@@ -22,11 +22,23 @@ include("ncm_algorithm.jl")
 include("ncm_solver.jl")
 include("ncm_solution.jl")
 
-include("legacy.jl")
+include("simple_interface.jl")
 
 include("algorithms/newton.jl")
-include("algorithms/alternatingprojection.jl")
+include("algorithms/alternatingprojections.jl")
 include("algorithms/directprojection.jl")
+
+
+using PrecompileTools: @compile_workload
+
+@compile_workload begin
+    for T in (Float16, Float32, Float64)
+        A = rand(T, 4, 4)
+        nearest_cor(A, Newton())
+        nearest_cor(A, AlternatingProjections())
+        nearest_cor(A, DirectProjection())
+    end
+end
 
 
 export
