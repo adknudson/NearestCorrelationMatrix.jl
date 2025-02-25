9e0c50dc6f6f25ab27c4a2440041f3bb27156529
adknudson <alexk.706@gmail.com>
2025-02-24 18:30:59 -0800

    Update compat helper workflow and removed compat from test deps

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,13 @@
+#!/usr/bin/env julia
+
 using SafeTestsets
 
-@time @safetestset "Quality Assurance" include("qa.jl")
-@time @safetestset "Code Formatting" include("format_check.jl")
-@time @safetestset "Utilities" include("internals.jl")
-@time @safetestset "Common Solve API" include("api.jl")
-@time @safetestset "Simple API" include("simple_api.jl")
-@time @safetestset "Algorithms" include("algorithms.jl")
-@time @safetestset "JuMP Extension" include("jump.jl")
+@time begin
+    @time @safetestset "Quality Assurance" include("qa.jl")
+    @time @safetestset "Code Formatting" include("format_check.jl")
+    @time @safetestset "Utilities" include("internals.jl")
+    @time @safetestset "Common Solve API" include("api.jl")
+    @time @safetestset "Simple API" include("simple_api.jl")
+    @time @safetestset "Algorithms" include("algorithms.jl")
+    @time @safetestset "JuMP Extension" include("jump.jl")
+end
