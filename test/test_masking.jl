64356ca2d87d8430a02d1b7a17099b8303999e8f
Alex Knudson <alexk.706@gmail.com>
2026-09-13 01:03:13 -0700

    relax the check on the eigenvalues

--- test/test_masking.jl
+++ test/test_masking.jl
@@ -23,7 +23,11 @@ for algtype in masking_algs, T in supported_types
         @test solver.mask !== nothing
         @test Base.mightalias(solver.A_orig, A) == false
         sol = solve!(solver)
-        @test_iscorrelation sol.X
+        @test isapprox(sol.X, sol.X')
+        @test isapprox(diag(sol.X), ones(T, size(sol.X, 1)))
+        @test all(x -> prevfloat(-one(T)) <= x <= nextfloat(one(T)), sol.X)
+        evals = eigvals(sol.X)
+        @test all(>=(-sqrt(sqrt(eps(T)))), evals)
         @test sol.X[m] == A[m]
     end
 end
