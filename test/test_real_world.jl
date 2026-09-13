93a7485e07cd3854cacd58fcb85736ef4209c9e3
Alex Knudson <alexk.706@gmail.com>
2026-09-13 00:18:51 -0700

    updated real world test structure

--- test/test_real_world.jl
+++ test/test_real_world.jl
@@ -1,5 +1,7 @@
 using Test
+using InteractiveUtils
 using NearestCorrelationMatrix
+import NearestCorrelationMatrix as NCM
 using NearestCorrelationMatrix.Internals: iscorrelation
 
 include("Datasets.jl")
@@ -8,106 +10,121 @@ using .Datasets
 include("CustomTestMacros.jl")
 using .CustomTestMacros
 
-@testset "BCCD16" begin
-    A = bccd16()
-    @test !iscorrelation(A)
-    nearest_cor!(A)
-    @test_iscorrelation A
-end
+internal_algtypes = setdiff(subtypes(NCMAlgorithm), (JuMPAlgorithm,))
+fast_algtypes = (Newton, DirectProjection)
 
-@testset "BEYU11" begin
-    A = beyu11()
-    @test !iscorrelation(A)
-    nearest_cor!(A)
-    @test_iscorrelation A
-end
+for alg in internal_algtypes
+    @info "Working on $alg"
 
-@testset "BHWI01" begin
-    A = bhwi01()
-    @test !iscorrelation(A)
-    nearest_cor!(A)
-    @test_iscorrelation A
-end
+    @testset "$(NCM.alg_name(alg))" begin
+        @testset "BEYU11" begin
+            A = beyu11()
+            @test !iscorrelation(A)
+            nearest_cor!(A, alg)
+            @test_iscorrelation A
+        end
 
-@testset "COR1399" begin
-    A = cor1399()
-    @test !iscorrelation(A)
-    nearest_cor!(A)
-    @test_iscorrelation A
-end
+        @testset "BHWI01" begin
+            A = bhwi01()
+            @test !iscorrelation(A)
+            nearest_cor!(A, alg)
+            @test_iscorrelation A
+        end
 
-@testset "COR3120" begin
-    A = cor3120()
-    @test !iscorrelation(A)
-    nearest_cor!(A)
-    @test_iscorrelation A
-end
+        @testset "COR1399" begin
+            A = cor1399()
+            @test !iscorrelation(A)
+            nearest_cor!(A, alg)
+            @test_iscorrelation A
+        end
 
-@testset "FING97" begin
-    # unmasked
-    A, _ = fing97()
-    @test !iscorrelation(A)
-    nearest_cor!(A)
-    @test_iscorrelation A
+        if alg ∈ fast_algtypes
+            @testset "BCCD16" begin
+                A = bccd16()
+                @test !iscorrelation(A)
+                nearest_cor!(A, alg)
+                @test_iscorrelation A
+            end
 
-    A, m = fing97()
-    @test !iscorrelation(A)
-    B = nearest_cor(A, AlternatingProjections(); mask = m)
-    @test_iscorrelation B
-    @test B[m] == A[m]
-end
+            @testset "COR3120" begin
+                A = cor3120()
+                @test !iscorrelation(A)
+                nearest_cor!(A, alg)
+                @test_iscorrelation A
+            end
+        end
 
-@testset "HIGH02" begin
-    A = high02()
-    @test !iscorrelation(A)
-    nearest_cor!(A)
-    @test_iscorrelation A
-end
+        @testset "FING97" begin
+            # unmasked
+            A, _ = fing97()
+            @test !iscorrelation(A)
+            nearest_cor!(A, alg)
+            @test_iscorrelation A
 
-@testset "MMB13" begin
-    A = mmb13()
-    @test !iscorrelation(A)
-    nearest_cor!(A)
-    @test_iscorrelation A
-end
+            if NCM.supports_mask(alg)
+                A, m = fing97()
+                @test !iscorrelation(A)
+                B = nearest_cor(A, alg; mask = m)
+                @test_iscorrelation B
+                @test B[m] == A[m]
+            end
+        end
 
-@testset "TEC03" begin
-    A = tec03()
-    @test !iscorrelation(A)
-    nearest_cor!(A)
-    @test_iscorrelation A
-end
+        @testset "HIGH02" begin
+            A = high02()
+            @test !iscorrelation(A)
+            nearest_cor!(A, alg)
+            @test_iscorrelation A
+        end
 
-@testset "TYDA99R1" begin
-    A = tyda99r1()
-    @test !iscorrelation(A)
-    nearest_cor!(A)
-    @test_iscorrelation A
-end
+        @testset "MMB13" begin
+            A = mmb13()
+            @test !iscorrelation(A)
+            nearest_cor!(A, alg)
+            @test_iscorrelation A
+        end
 
-@testset "TYDA99R2" begin
-    A = tyda99r2()
-    @test !iscorrelation(A)
-    nearest_cor!(A)
-    @test_iscorrelation A
-end
+        @testset "TEC03" begin
+            A = tec03()
+            @test !iscorrelation(A)
+            nearest_cor!(A, alg)
+            @test_iscorrelation A
+        end
 
-@testset "TYDA99R3" begin
-    A = tyda99r3()
-    @test !iscorrelation(A)
-    nearest_cor!(A)
-    @test_iscorrelation A
-end
+        @testset "TYDA99R1" begin
+            A = tyda99r1()
+            @test !iscorrelation(A)
+            nearest_cor!(A, alg)
+            @test_iscorrelation A
+        end
 
-@testset "USGS13" begin
-    A, _ = usgs13()
-    @test !iscorrelation(A)
-    nearest_cor!(A)
-    @test_iscorrelation A
+        @testset "TYDA99R2" begin
+            A = tyda99r2()
+            @test !iscorrelation(A)
+            nearest_cor!(A, alg)
+            @test_iscorrelation A
+        end
 
-    A, m = usgs13()
-    @test !iscorrelation(A)
-    B = nearest_cor(A, AlternatingProjections(tau = 1.0e-6); mask = m)
-    @test_iscorrelation B
-    @test B[m] == A[m]
+        @testset "TYDA99R3" begin
+            A = tyda99r3()
+            @test !iscorrelation(A)
+            nearest_cor!(A, alg)
+            @test_iscorrelation A
+        end
+
+        @testset "USGS13" begin
+            A, _ = usgs13()
+            @test !iscorrelation(A)
+            nearest_cor!(A, alg)
+            @test_iscorrelation A
+
+            if NCM.supports_mask(alg)
+                A, m = usgs13()
+                @test !iscorrelation(A)
+                B = nearest_cor(A, alg; mask = m)
+                @test_iscorrelation B
+                @test B[m] == A[m]
+            end
+        end
+    end
 end
